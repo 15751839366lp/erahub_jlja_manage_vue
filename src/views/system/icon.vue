74e93a00e4564f2628ec7471b21fb3503d9230cf
李彭 <15751839366@163.com>
2021-10-17 20:28:21 +0800

    fix

--- src/views/system/icon.vue
+++ src/views/system/icon.vue
@@ -3,7 +3,7 @@
         <!-- 面包导航 -->
         <el-breadcrumb separator="/" style="padding-left:10px;padding-bottom:10px;font-size:12px;">
             <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
-            <el-breadcrumb-item>其他管理</el-breadcrumb-item>
+            <el-breadcrumb-item>系统管理</el-breadcrumb-item>
             <el-breadcrumb-item>图标管理</el-breadcrumb-item>
         </el-breadcrumb>
         <el-card>
